31fe00f531d40baf848d782ff498d603a5d0f44a
Bob Guo <boblao0714@gmail.com>
2020-03-31 14:40:07 +0800

    File currption

--- python/afp.py
+++ python/afp.py
@@ -4,10 +4,17 @@
 def sum_of_word(word):
     sum = 0 
     for char in word:
-        sun += ord(char) - 96 # a starts with 97 but we need to def a as 1 so -96
+        sum += ord(char) - 96 # a starts with 97 but we need to def a as 1 so -96
     return sum
 
-with open("words_alpha.txt", "r") as file:
-    for word in file.readlines():
-        if sum_of_word(word) == 100:
-            print(word)
+print(sum_of_word("latitude"))
+print(sum_of_word("Latitude"))
+print(sum_of_word("precision"))
+print(sum_of_word("Precision"))
+
+with open("result.txt", "w") as result: ## Generating result.txt as result
+    with open("words_alpha.txt", "r") as file:
+        for word in file.readlines():
+            if sum_of_word(word.strip()) == 100:
+                print(word)
+                result.write(word) # Save results to the file, which lefts with 3700 or so
