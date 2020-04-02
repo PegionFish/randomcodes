57e8ebbb7d157d047d56c775df10a96397fa66c4
Bob Guo <boblao0714@gmail.com>
2020-04-02 13:39:33 +0800

    Indepent Repo https://github.com/PegionFish/Brother-Tiger-Vs-Shamate-Tuanzhang

--- python/HuGe vs ShamateTuanzhang/setting.py
+++
@@ -1,6 +0,0 @@
-class Settings():
-    def __init__(self):    
-# Default configurations for game
-        self.width = 1366
-        self.height = 800
-        self.bg_color = (40, 80, 100) # 我也喜欢二次元
